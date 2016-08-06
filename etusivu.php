6c34ed9244404bf01c26f847bd3b03d36d248fe1
Johanna Savinen <johanna.savinen@ilmedia.fi>
2016-08-06 19:06:37 +0300

    Add hr styles

--- etusivu.php
+++ etusivu.php
@@ -12,24 +12,24 @@ Template Name: Etusivu / Home
 </div>
 
 <div class="row">
-		  <div class="small-6 large-6 columns">
+		  <div class="small-12 large-6 columns">
 		  	<h2>Uutiset</h2>
 		  	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
 		  </div>
-		  <div class="small-6 large-6 columns">
+		  <div class="small-12 large-6 columns">
 		  	<h2>Tietoa Intiasta</h2>
 		  	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
 		  </div>
 </div>
 
-<hr>
+<hr class="vaaka">
 
 <div class="row">
-		  <div class="small-6 large-6 columns">
+		  <div class="small-12 large-6 columns">
 		  	<h2>Tulevat tapahtumat</h2>
 		  	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
 		  </div>
-		  <div class="small-6 large-6 columns">
+		  <div class="small-12 large-6 columns">
 		  	<h2>Haluatko jäseneksi</h2>
 		  	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
 		  </div>
