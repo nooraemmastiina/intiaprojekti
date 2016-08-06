94b4e9ee0c34a60fee95fef23b0c0a65e4283df7
Johanna Savinen <johanna.savinen@ilmedia.fi>
2016-08-06 14:22:32 +0300

    Created Etusivu

---
+++ etusivu.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Etusivu / Home
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="row">
+		  <div class="small-6 large-6 columns">
+		  	<h2>Uutiset</h2>
+		  	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
+		  </div>
+		  <div class="small-6 large-6 columns">
+		  	<h2>Tietoa Intiasta</h2>
+		  	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
+		  </div>
+</div>
+
+<hr>
+
+<div class="row">
+		  <div class="small-6 large-6 columns">
+		  	<h2>Tulevat tapahtumat</h2>
+		  	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
+		  </div>
+		  <div class="small-6 large-6 columns">
+		  	<h2>Haluatko jäseneksi</h2>
+		  	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
+		  </div>
+</div>
+
+
+<?php get_footer(); ?>
