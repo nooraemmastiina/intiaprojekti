d061497ccf8dcd966f8bb9d9f0bd8dd8f6bcd4ba
Johanna Savinen <johanna.savinen@ilmedia.fi>
2016-08-06 19:30:13 +0300

    Add vertical hr styles

--- etusivu.php
+++ etusivu.php
@@ -12,9 +12,10 @@ Template Name: Etusivu / Home
 </div>
 
 <div class="row">
-		  <div class="small-12 large-6 columns">
+		  <div class="small-12 large-6 columns pysty">
 		  	<h2>Uutiset</h2>
 		  	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
 		  </div>
 		  <div class="small-12 large-6 columns">
 		  	<h2>Tietoa Intiasta</h2>
@@ -25,7 +26,7 @@ Template Name: Etusivu / Home
 <hr class="vaaka">
 
 <div class="row">
-		  <div class="small-12 large-6 columns">
+		  <div class="small-12 large-6 columns pysty">
 		  	<h2>Tulevat tapahtumat</h2>
 		  	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum lacinia ligula, sed pulvinar sem interdum vel. Donec eget massa in erat congue pretium. Curabitur eu aliquet leo. Nulla eu iaculis ipsum, nec aliquam augue. Donec consequat massa massa, vitae sodales metus hendrerit a. Quisque ac mauris sit amet risus molestie tincidunt vitae a diam. Vestibulum non luctus neque.
 		  </div>
