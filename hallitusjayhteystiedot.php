831d1372419e9d07f243ad2f439958632d1ef99a
Johanna Savinen <johanna.savinen@ilmedia.fi>
2016-08-07 22:00:10 +0300

    Hallitus ja yhteystiedot

---
+++ hallitusjayhteystiedot.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Template Name: Hallitus ja yhteystiedot / Contacts
+*/
+?>
+
+<?php get_header(); ?>
+
+<h1><?php the_title(); ?></h1>
+</div>
+
+<div class="row" data-equalizer>
+      <div class="small-12 large-6 columns pysty" data-equalizer-watch>
+        <h2>Hallitus</h2>
+              <p>Suomi-Intia-Seuraa johtaa hallitus, joka valitaan vuosittain seuran jäsenille 
+              avoimessa syyskokouksessa. Kutsu syyskokoukseen lähetetään kaikille jäsenille. Kaikilla seuran jäsenillä on kokouksessa äänioikeus.
+
+              Vuonna 2016 hallituksen puheenjohtajana toimii Markku Lemmetty, 
+              varapuheenjohtajana Tuomas Heino. Muut hallituksen jäsenet ovat Shefali Arora, Tiina Karjalainen, Ashu Patpatia ja Marjatta Rasi.
+              </p>
+
+      </div>           
+
+      <div class="small-12 large-6 columns" data-equalizer-watch>
+      <h2>Yhteystiedot</h2>  
+      <div class="small-12 large-7 columns">             
+          <p>Suomi-Intia-Seura ry.</p>
+          <p> Opastinsilta 9 B 39, 00520 Helsinki</p>
+          <p> info@suomiintiaseura.fi</p>
+          <p>Puheenjohtaja Markku Lemmetty</p>
+          <p>mol.lemmetty@pp.inet.fi</p>
+          <p>puh. 0400 411 036</p>
+      </div>
+
+            <div class="small-12 large-5 columns">             
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kaari_ikkuna.png">
+      </div>
+
+      </div>
+</div>
+
+
+<?php get_footer(); ?>
