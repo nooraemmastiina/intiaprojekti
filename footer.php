c92409503b0eec023397da95f1b49bdc51306e21
Johanna Savinen <johanna.savinen@ilmedia.fi>
2016-08-21 20:24:42 +0300

    Footer

--- footer.php
+++ footer.php
@@ -1,12 +1,16 @@
 					<footer class="footer" role="contentinfo">
 						<div id="inner-footer" class="row">
-							<div class="large-12 medium-12 columns">
+							<div class="large-9 medium-12 columns">
 								<nav role="navigation">
 		    						<?php joints_footer_links(); ?>
 		    					</nav>
+		    					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
 		    				</div>
-							<div class="large-12 medium-12 columns">
-								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+							<div class="large-3 medium-12 columns">
+						          Suomi-Intia-Seura ry.<br>
+						          Opastinsilta 9 B 39, 00520 Helsinki<br>
+						          info@suomiintiaseura.fi
+
 							</div>
 						</div> <!-- end #inner-footer -->
 					</footer> <!-- end .footer -->
