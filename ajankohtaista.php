c8fdca1b75e6bd138f8dd1e0018287b75ffb770a
Johanna Savinen <johanna.savinen@ilmedia.fi>
2016-09-03 13:14:14 +0300

    Style changes

--- ajankohtaista.php
+++ ajankohtaista.php
@@ -36,9 +36,12 @@ Template Name: Ajankohtaista / News
             ?>
             <h3 class="entry-title"><?php the_title(); ?></h3>
             <div class="entry-content">
-              <?php the_content(); ?>
-              <?php if( get_field('ilmoittaudu_linkki') ): ?> <a class="small button" href="<?php the_field('ilmoittaudu_linkki')?>">Ilmoittaudu</a><?php endif; ?><br>
               <?php the_post_thumbnail('large', array('class' => 'large')); ?>
+              <br>
+              <?php if( get_field('ilmoittaudu_linkki') ): ?> <a class="small button" href="<?php the_field('ilmoittaudu_linkki')?>">Ilmoittaudu</a><?php endif; ?>
+              <?php the_content(); ?>
+             
+              
             </div>
             <?php endwhile;?>
 
